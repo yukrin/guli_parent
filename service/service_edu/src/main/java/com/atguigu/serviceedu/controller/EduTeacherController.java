cfa13ce164eaa2423d21ace52211028039b7c9d1
chenxin <Yozo168*>
2021-11-19 23:46:57 +0800

    11

---
+++ service/service_edu/src/main/java/com/atguigu/serviceedu/controller/EduTeacherController.java
@@ -0,0 +1,21 @@
+package com.atguigu.serviceedu.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author atguigu
+ * @since 2021-11-19
+ */
+@RestController
+@RequestMapping("/serviceedu/teacher")
+public class EduTeacherController {
+
+}
+
